9eac571f848f072a9fc91fe3b3b85c0df528deb3
AdamS839 <alexanderdev@yahoo.com>
2025-06-03 13:05:31 +0300

    added comments on main.cpp about using auto

--- main.cpp
+++ main.cpp
@@ -12,8 +12,7 @@ int main() {
     std::string line;
     std::cout << "Enter command to call: ";
     
-    while (true)
-    {
+    while (true){
         std::cout << "> ";
         if(!std::getline(std::cin, line)) break;
         if(line.empty()) continue;
@@ -22,7 +21,10 @@ int main() {
         std::string restline = getRestOfLine(line, command);
 
         // implement map for execute()
+        //instead of auto - use: std::map<std::string, std::function<Command*(const std::string &, std::vector<Figure*> &)>> commandFactory = createCommandFactory(manager);
         auto commandFactory = createCommandFactory(manager);
+
+        //instead of auto - use: std::map<std::string, std::function<Command*(const std::string&, std::vector<Figure*>&)>>::iterator it = commandFactory.find(command);
         auto it = commandFactory.find(command);
         if (it != commandFactory.end()) {
             Command* cmd = it->second(restline, manager.getFigures());
@@ -30,10 +32,9 @@ int main() {
                 cmd->execute();
                 delete cmd;
             }
-        } else {
+        } else{
             std::cerr << "Unknown command: \"" << command << "\"\n";
         }
-
     }
 
     return 0;
